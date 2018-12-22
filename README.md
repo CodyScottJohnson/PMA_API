e8c24ae5f734af5d901f2479f688e17305a08cf3
Cody Johnson <cody@anpac.net>
2018-12-22 14:33:18 -0700

    Updated README

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
+<h1 align="center">
+  <br>
+  <a href="https://ldsmba.codyscottjohnson.com"><img src="https://raw.githubusercontent.com/codyscottjohnson/Product-Management-API/master/docs/imgs/PMA-Logo.png" alt="U LDS MBA" width="200"></a>
+  <br>
+  <h4 align="center">Backend for University of Utah Product Management Association</h4>
+</h1>
 # Product Management API
---
+++ pkg/protocol/grpc/server.go
@@ -0,0 +1,40 @@
+package grpc
+
+import (
+	"context"
+	"net"
+	"os"
+	"os/signal"
+
+	"google.golang.org/grpc"
+
+	"Product-Management-API/pkg/api/v1"
+)
+
+//RunServer - Start GRPC Server
+func RunServer(ctx context.Context, API v1.ProjectServiceServer, port string) error {
+	listen, err := net.Listen("tcp", ":"+port)
+	if err != nil {
+		return err
+	}
+
+	//Register Service
+	server := grpc.NewServer()
+	v1.RegisterToDoServiceServer(server, API)
+
+	//Gracefull Shutdown
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	go func() {
+		for range c {
+			log.PrintLn("shutting down gRPC server...")
+
+			server.GracefulStop()
+
+			<-ctx.Done()
+		}
+	}()
+	log.Println("starting gRPC server...")
+	return server.Serve(listen)
+
+}
